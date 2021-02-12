4c0457cb8c0a54f1f450e0b94d0dd9e1b9b5de49
ahalyabayyana <ahalyabayyana@gmail.com>
2021-02-12 14:24:52 -0500

    Merge remote-tracking branch 'origin/master'

--- Fun_yield_nxt.py
+++ Fun_yield_nxt.py
@@ -1,25 +1,52 @@
 import numpy as np
+
+
 def add(*var):
     for i in var:
-        yield i%2
-var=np.arange(0,1000,1)
-add_gen=add(var)
+        yield i % 2
+
+
+# var = np.arange(0, 1000, 1)
+# add_gen = add(var)
+
+
 # print("printing the output using next method",next(add(var)))
 # print("Converting into a list :",list(add_gen))
 
 
 def outer_fn(var):
     print("Welcome to outer function")
+
     def inner_fn():
-        print("Welcome to inner function",var)
+        print("Welcome to inner function", var)
+
     inner_fn()
-outer_fn("varaiable is hello")
+
+
+# outer_fn("varaiable is hello")
+
 
 def outer_fn1(var):
-    print("Welcome to outer function",var)
+    print("Welcome to outer function", var)
+
     def inner_fn1(var1):
-        print("inner function varaibale:",var1 + "outer function varaiable is :",var)
+        print("inner function varaibale:", var1 + "outer function varaiable is :", var)
+
     return inner_fn1
 
-fn_var=outer_fn1("var is outer")
-print(fn_var("var is for inner fn"))
+
+# fn_var = outer_fn1("var is outer")
+#print(fn_var("var is for inner fn"))
+
+
+def gen_fn(n):
+
+    for i in range(0,n):
+        yield i
+
+
+
+
+g = gen_fn(6)
+
+print(next(g))
